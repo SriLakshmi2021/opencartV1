0d45ae4e7b7f47e2c71f1ebc57d263cb5509f2fe
SriLakshmi2021 <81348845+SriLakshmi2021@users.noreply.github.com>
2021-03-26 18:04:32 -0700

    Update TC_006_Addcart.java

--- src/test/java/testCases/TC_006_Addcart.java
+++ src/test/java/testCases/TC_006_Addcart.java
@@ -1,2 +1,9 @@
 
 //This is my ADD cart Test case
+//This is my ADD cart Test case
+//This is my ADD cart Test case
+//This is my ADD cart Test case
+//This is my ADD cart Test case
+//This is my ADD cart Test case
+//This is my ADD cart Test case
+//This is my ADD cart Test case//This is my ADD cart Test case
